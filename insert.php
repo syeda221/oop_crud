b290783086075c7c856adf8a024a17367b9a542d
qunoot zehra <qunootzehra21@gmail.com>
2026-01-30 17:35:50 +0500

    changes

--- insert.php
+++ insert.php
@@ -8,6 +8,7 @@ $table = new allfunc($database);
         $age = $_POST['age'];
         if($table->insert($name , $age)){
             echo "<script>alert('data inserted successfully')</script>";
+            header("Location:index.php")
         }
         else {
             echo "data inserting issue";
@@ -15,3 +16,15 @@ $table = new allfunc($database);
     }
 
 ?>
+    <div class="buttons">
+        <a href="index.php"><button>Go Back</button></a>
+
+    </div>
+    <div class="insert"> <h1>Insert new user </h1><br>
+    <form method="post">
+        Name: <br>
+        <input type="text" name="name" ><br>
+        Email: <br>
+        <input type="text" name="age" ><br>
+        <input type="submit" value="Insert" name="submit">
+    </form></div>
