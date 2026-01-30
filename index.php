9d0c5dea517e69f0a39c69c1a7d000cb52a7a10f
qunoot zehra <qunootzehra21@gmail.com>
2026-01-30 17:47:50 +0500

    chcnages

--- index.php
+++ index.php
@@ -10,9 +10,19 @@ $table = new allfunc($database);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Data </title>
-    
+    <style>
+         table{
+            width:700px;
+            border:1px solid;
+         }
+         tr,th{
+            border:1px solid;
+
+         }
+    </style>
 </head>
 <body>
+    <a href="insert.php"><button>Insert Data</button></a>
     <h1>All Data</h1>
     <table>
         <tr>
@@ -28,12 +38,12 @@ foreach($users as $user){
 ?>
 <tr>
     <td><?=$user['id']?></td>
-    <td><?=$user['NAME']?></td>
+    <td><?=$user['name']?></td>
     <td><?=$user['age']?></td>
-    <td>DELETE</td>
-    <td>UPDATE</td>
+   <td><a href="#">DELETE</a></td>
+   <td><a href="#">UPDATE</a></td>
 </tr>
-  <?php} ?>  
+  <?php  } ?>  
     </table>
 
 </body>
