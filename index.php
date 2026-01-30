b0704f3bca57e7a0b03b6dd7e88a1fafb7e5e6e9
qunoot zehra <qunootzehra21@gmail.com>
2026-01-30 17:01:03 +0500

    updaed

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ include 'functions.php';
 $database = (new database)->connect();
 $table = new allfunc($database);
     if(isset($_POST['submit'])){
-        $name = $_POST['name'];
-        $age = $_POST['age'];
+        $name = sanitize($_POST['name']);
+        $age = sanitize($_POST['age']);
         if($table->insert($name , $age)){
             echo "<script>alert('data inserted successfully')</script>";
         }
