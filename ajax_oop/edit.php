ef2f9c2aef3616b5c5ea994a409920dc4f45c2cb
qunoot zehra <qunootzehra21@gmail.com>
2026-02-06 16:46:42 +0500

    chnags

--- ajax_oop/edit.php
+++ ajax_oop/edit.php
@@ -6,13 +6,16 @@ $database =(new database())->db();
 $table = new crud($database);
 $id=$_POST["id"];
 
-$fetch_id = $table->edit_fetch($id);
+$fetch_id = $table->edit_fetch($id)->fetch();
 ?>
  <h5>edit Data</h5>
 <?php
 
 ?>
+Name: <br>
      <input type="text" name="name" value="<?=$fetch_id['name']?>" id="edit-name"><br><br>
+    Age:
+    <br>
         <input type="number" name="age" value="<?=$fetch_id['age']?>" id="edit-age"><br><br>
         <input type="submit" value="edit"  class="edit-btn">
         <h1 class="cross">X</h1>
