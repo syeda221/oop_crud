83d4b9b9684491fbb09cd8f54ae1b9678126974d
qunoot zehra <qunootzehra21@gmail.com>
2026-02-06 16:50:43 +0500

    chanfes

--- ajax_oop/edit.php
+++ ajax_oop/edit.php
@@ -6,16 +6,18 @@ $database =(new database())->db();
 $table = new crud($database);
 $id=$_POST["id"];
 
-$fetch_id = $table->edit_fetch($id)->fetch();
+$fetch_id = $table->edit_fetch($id)->fetch(PDO::FETCH_ASSOC);
 ?>
- <h5>edit Data</h5>
+ <h3>edit Data</h3>
+ <br>
 <?php
 
 ?>
-Name: <br>
-     <input type="text" name="name" value="<?=$fetch_id['name']?>" id="edit-name"><br><br>
-    Age:
-    <br>
+<h6>Name</h6> 
+     <input type="text" name="name" value="<?=$fetch_id['name']?>" id="edit-name">
+     <br>
+   <h6>Age</h6>
+    
         <input type="number" name="age" value="<?=$fetch_id['age']?>" id="edit-age"><br><br>
         <input type="submit" value="edit"  class="edit-btn">
         <h1 class="cross">X</h1>
