d81e13b1c75fc55b3ab9b4ebb90a4ef724e79b11
qunoot zehra <qunootzehra21@gmail.com>
2026-02-11 01:44:46 +0500

    chnages

--- oop_crud/index.php
+++ oop_crud/index.php
@@ -1,10 +1,13 @@
+<?php
+include '../ajax_oop/allmeth.php';
+include '../ajax_oop/connect.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Simple CRUD Table</title>
     <link rel="stylesheet" href="../ajax_oop/style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
 <body>
 
@@ -42,101 +45,7 @@
        
 </div>
 </div>
-<script>
-    $(document).ready(function(){
-        //fetch function 
-        function fetchData(){
-            $.ajax({
-                url: "fetch.php",
-                type: "POST",
-                success: function(data){
-                    $("tbody").html(data);
-                }
-                });
-            }
-         fetchData();
-         //form show
-        $(".btn.add").on('click', function(){
-            $("#insert-data").show();
-        });  
-        $(".cross").on('click', function(){
-            $("#insert-data").hide();
-        });
 
-        //insert function
-        function insert(e){
-            e.preventDefault();
-            let name = $("#name").val();
-            let age = $("#age").val();
-            $.ajax({
-                url: "insert.php",
-                type: "POST",
-                data: {"name":name,"age":age},
-                success: function(data){
-                  alert(data);
-                  $("#insert-data")[0].reset();
-                  $("#insert-data").hide();
-                  fetchData();
-                }
-            });
-        }
-        //insert funxtio  call
-       $(".add-form").on('submit', insert);
 
-       //dlete function
-        function deletefun(e){
-            e.preventDefault();
-            var id = $(this).data("did");
-            if(!id) return;
-            $.ajax({
-                url: "delete.php",
-                type: "POST",
-                data: {"id": id},
-                success: function(data){
-                    alert(data);
-                    fetchData();
-
-                }
-            });
-        }
-        //delete function call
-        $(document).on('click','.delete', deletefun);
-
-        $(document).on('click','.edit',function(){
-            let id = $(this).data('eid');
-            $("#update-data").show();
-            $.ajax({
-                url: "edit.php",
-                type: "POST",
-                data: {"id":id},
-                success: function(data){
-                $("#update-data").html(data);
-                // show the injected form (the CSS hides forms by default)
-                $("#update-data form").show();
-                }
-            });
-        })
-        $(document).on('submit','#edit-form',function(e){
-            e.preventDefault();
-            $.ajax({
-                url: "update.php",
-                type: "POST", 
-                data: $(this).serialize(),
-                success: function(data){
-                    alert(data);
-                $("#update-data form").hide();
-
-                    fetchData();
-
-                }
-            })
-        })
-         $(document).on('click','.cross',function(){
-            $("#update-data").hide();
-        })
-        //update function 
-        
-    });
-</script>
 </body>
 </html>
