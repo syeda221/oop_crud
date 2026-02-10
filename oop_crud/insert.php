a80f6805e77fb2e08a59e74de1d444877edc9e6e
qunoot zehra <qunootzehra21@gmail.com>
2026-02-11 02:19:32 +0500

    chnages

--- oop_crud/insert.php
+++ oop_crud/insert.php
@@ -3,14 +3,22 @@ include 'all.php';
 include '../ajax_oop/connect.php';
 $database = (new database)->db();
 $table = new crud($database);
-
+if(isset($_POST['add']))
+{
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $image = $_POST['name']['name'];
+    $name = $_POST['name'];
+}
 ?>
 
-<form class="add-form" id="insert-data" method="post">
+<form class="add-form" style="opacity:1;" id="insert-data" method="post">
         <h5>Add Data</h5>
         <input type="text" name="name" id="name"><br><br>
         <input type="number" name="age" id="age"><br><br>
-        <input type="submit" value="Add" class="add-btn">
+        <input type="file" name="image" id="age"><br><br>
+        <input type="password" name="password" id="age"><br><br>
+        <input type="submit" name="add" value="Add" class="add-btn">
         <h1 class="cross">X</h1>
 
     </form>
