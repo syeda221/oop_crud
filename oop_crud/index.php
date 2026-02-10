64e47b169ff6d27a03db80ef4298cf5f97152167
qunoot zehra <qunootzehra21@gmail.com>
2026-02-11 02:01:47 +0500

    changes

--- oop_crud/index.php
+++ oop_crud/index.php
@@ -1,12 +1,16 @@
 <?php
 include '../ajax_oop/allmeth.php';
 include '../ajax_oop/connect.php';
+$database = (new database)->db();
+$table =  crud($database);
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Simple CRUD Table</title>
+    <title>Simple oop CRUD Table</title>
     <link rel="stylesheet" href="../ajax_oop/style.css">
 </head>
 <body>
@@ -23,14 +27,28 @@ include '../ajax_oop/connect.php';
                 <th>ID</th>
                 <th>Name</th>
                 <th>Age</th>
+                <th>Image</th>
+                <th>Password</th>
                 <th>Actions</th>
             </tr>
         </thead>
 
         <tbody>
-          
+          <?php  
+          $data = $table->getAll()->fetch();
+          foreach($data as $d){
 
-          
+          ?>
+ <tr>
+                <td ><?=htmlspecialchars($id, ENT_QUOTES, 'UTF-8')?></td>
+                <td><?=htmlspecialchars($name, ENT_QUOTES, 'UTF-8')?></td>
+                <td><?=htmlspecialchars($age, ENT_QUOTES, 'UTF-8')?></td>
+                <td>
+                    <button class="btn edit"  id="edit"><a href="edit.php?id=<?php $d['id'] ?>">Edit</a></button>
+                    <button class="btn edit"  id="delete"><a href="delete.php?id=<?php $d['id'] ?>">Delete</a></button>
+                </td>
+            </tr>
+          <?php } ?>
         </tbody>
     </table>
     <form class="add-form" id="insert-data" method="post">
