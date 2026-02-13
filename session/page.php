38454263bb242e5526fe79d0488a85360645f4a7
qunoot zehra <qunootzehra21@gmail.com>
2026-02-13 20:42:23 +0500

    chanfes

--- session/page.php
+++ session/page.php
@@ -1,9 +1,10 @@
 <?php
-if(isset($_POST['username'] && $_POST['password'])){
+if($_POST['username'] && $_POST['password']){
     session_start();
     $_SESSION['username'] = $_POST['username'];
     $_SESSION['password'] =  $_POST['password'];
     echo "welcome ". $_SESSION['username'];
+    echo "<br><button><a href='logout.php'>logout</a></button>";
 }else{
     header("location:login.php");
 }
