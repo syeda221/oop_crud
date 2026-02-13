47540f9980c77daa91bb56c19add7dda9b71704b
qunoot zehra <qunootzehra21@gmail.com>
2026-02-13 20:26:12 +0500

    chnafes

--- session/login.php
+++ session/login.php
@@ -6,6 +6,12 @@
     <title>Login</title>
 </head>
 <body>
-    
+    <form action="page.php" method="post">
+        Username:
+        <input type="text" name="username"><br><br>
+        Password:
+        <input type="password" name="password"><br><br>
+        <input type="submit" name="login" value="Login">
+    </form>
 </body>
 </html>
