e915beb17c86708280b355a6ee59e1738df2d755
qunoot zehra <qunootzehra21@gmail.com>
2026-02-13 20:32:30 +0500

    chnages

--- session/page.php
+++ session/page.php
@@ -1,9 +1,10 @@
 <?php
 if(isset($_POST['username'] && $_POST['password'])){
+    session_start();
     $name = $_POST['username'];
     $password =  $_POST['password'];
     echo "welcome ". $name;
 }else{
-    
+    header("location:login.php");
 }
 ?>
